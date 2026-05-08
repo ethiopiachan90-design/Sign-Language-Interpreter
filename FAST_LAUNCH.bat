abf98cb4d0cfaae0c7c681a9fbc1a9df19e2421e
ethiopiachan90-design <ethiopiachan90@gmail.com>
2026-05-08 23:49:38 +0300

    Added fast launch scripts for presentation

---
+++ FAST_LAUNCH.bat
@@ -0,0 +1,6 @@
+@echo off
+title Sign Language Interpreter - FAST LAUNCH
+cd /d "c:\Users\mutentiza\Videos\Sign-Language-To-Text-and-Speech-Conversion"
+echo Launching...
+.\keras_env\Scripts\python.exe final_pred.py
+pause
